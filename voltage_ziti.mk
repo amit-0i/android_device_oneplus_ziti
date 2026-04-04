9d7f4d6ec701e8ca85e2b44e4f9539638444ce03
amit-0i <6329amit@gmail.com>
2026-04-05 00:21:43 +0530

    ziti: initial bringup for VoltageOS

--- voltage_ziti.mk
+++ voltage_ziti.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ziti device
 $(call inherit-product, device/oneplus/ziti/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ziti
+PRODUCT_NAME := voltage_ziti
 PRODUCT_DEVICE := ziti
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -30,3 +30,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=CPH2569 \
     SystemDevice=OP5953L1 \
     SystemName=CPH2569
+
+PRODUCT_MANUFACTURER_FOR_ATTESTATION := OnePlus
+PRODUCT_BRAND_FOR_ATTESTATION :=  OnePlus
+PRODUCT_DEVICE_FOR_ATTESTATION :=  ziti
+PRODUCT_NAME_FOR_ATTESTATION := OnePlus Nord CE3 5G
+PRODUCT_MODEL_FOR_ATTESTATION := CPH2569
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TORCH_STR_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
